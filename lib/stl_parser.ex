8c6e516c7e1ca46c7e1f4186ef4b84176d98a8c2
Faizaan Shamsi <faizaan@quantlayer.com>
2020-03-18 16:54:47 -0400

    Inital commit

---
+++ lib/stl_parser.ex
@@ -0,0 +1,18 @@
+defmodule StlParser do
+  @moduledoc """
+  Documentation for `StlParser`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> StlParser.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
